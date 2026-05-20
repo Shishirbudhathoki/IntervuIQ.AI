80f077466a13bb5844e4fe0a82309a4be6a5cf2b
Shishir Budhathoki <shishirbudhathoki4000@gmail.com>
2026-05-20 18:37:25 +0545

    Inital setup and database connected

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './config/connectDb.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+  res.json({msg : 'Hello World!'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+  connectDb();
+});
