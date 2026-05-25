3ab4c8301e37844090489a71546c0b856e895f32
Shishir Budhathoki <shishirbudhathoki4000@gmail.com>
2026-05-25 16:07:22 +0545

    Fixed type in css

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -7,7 +7,7 @@ function InterviewPage() {
     const [step, setStep] = useState(1);
     const [interviewData, setInterviewData] = useState(null);
     return (
-        <div className="min-h-screen bg-gtay-50">
+        <div className="min-h-screen bg-gray-50">
             {step === 1 && (
                 <Step1SetUp onStart={(data) => {
                     setInterviewData(data);
